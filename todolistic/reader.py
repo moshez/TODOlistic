83f0063a6a88d8445811266faeccfbfc3beb98e1
Moshe Zadka <moshez@divmod.com>
2012-08-09 07:12:43 -0700

    Basic code to do stuff

---
+++ todolistic/reader.py
@@ -0,0 +1,28 @@
+import os
+import json
+
+class TODOItem(object):
+    def __init__(self, due=None, title=None, notes=(), repeats=False, **kw):
+        self.due = due # For now -- parse into seconds-since-epoch later
+        self.title = title
+        self.notes = notes
+        self.repeats = bool(repeats)
+        self.metadata = kw
+    def __repr__(self):
+        d = self.__dict__.copy()
+        d.update(self.metadata)
+        return 'TODOItem(%r)' % d
+
+def readTODO(fname):
+    rawData = json.load(file(fname))
+    data = dict([(str(k), v) for k, v in rawData.iteritems()])
+    return TODOItem(**data)
+
+def readDirectory(dname):
+    ret = []
+    for fname in os.listdir(dname):
+        fname = os.path.join(dname, fname)
+        ret.append(readTODO(fname))
+    return ret
+
+
